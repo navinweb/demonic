d46cfdeaadaa64ba2a4e24b3315ac1ac2cb07d43
navinweb <anatoliykirzo@gmail.com>
2018-05-24 19:28:37 +0300

    add router, add redis support, add logger, add bodyparser

--- app/app.js
+++ app/app.js
@@ -1,9 +1,27 @@
 import Koa from 'koa';
+const logger = require('koa-morgan');
+const Router = require('koa-router');
+const bodyParser = require('koa-body');
+const redis = require('redis');
+const bluebird = require('bluebird');
+
+bluebird.promisifyAll(redis.RedisClient.prototype);
+const db = redis.createClient();
 
 const app = new Koa();
+const router = new Router();
 
-app.use(async (ctx) => {
-    ctx.body = '<h1>Summary Test</h1>';
+router.get('/', (ctx) => {
+    ctx.body = '<h1>Demonic</h1>';
 });
+
+router.post('/about', bodyParser(), async (ctx) => {
+    await db.setAsync('WISP', 'Hey Ho');
+    ctx.body = { data: ctx.request.body };
+});
+
+app
+.use(logger('tiny'))
+.use(router.routes());
 
 export default app;

5e2ba9022f11bb0bb3889e993594927e87ddaf62
navinweb <anatoliykirzo@gmail.com>
2018-06-13 21:55:38 +0300

    add learn test route

--- app/app.js
+++ app/app.js
@@ -11,17 +11,26 @@ const db = redis.createClient();
 const app = new Koa();
 const router = new Router();
 
-router.get('/', (ctx) => {
-    ctx.body = '<h1>Demonic</h1>';
+router.get('/', async ctx => {
+  ctx.body = '<h1>Demonic</h1>';
 });
 
-router.post('/about', bodyParser(), async (ctx) => {
-    await db.setAsync('WISP', 'Hey Ho');
-    ctx.body = { data: ctx.request.body };
+router.get('/about', async ctx => {
+  ctx.body = '<h1>Demonic About</h1>';
 });
 
-app
-.use(logger('tiny'))
-.use(router.routes());
+router.post('/about', bodyParser(), async ctx => {
+  await db.setAsync('WISP', 'Hey Ho');
+  ctx.body = { data: ctx.request.body };
+});
+
+router.get('/learn', async ctx => {
+  ctx.body = {
+    title: 'Chernobog!!!',
+    content: 'Test store'
+  };
+});
+
+app.use(logger('tiny')).use(router.routes());
 
 export default app;
